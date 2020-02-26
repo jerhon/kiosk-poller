d468365a1cf88cae2fc9d8a0da797cafa186e778
Jeremy Honl <jerhon@users.noreply.github.com>
2020-02-25 21:14:53 -0600

    Not refreshing on restart, use incognito for now.

--- setup/pi-plate.sh
+++ setup/pi-plate.sh
@@ -8,4 +8,4 @@ unclutter -idle 0.01 -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-/usr/bin/chromium-browser --disable-pinch --noerrdialogs --disable-infobars --overscroll-history-navigation=0 --check-for-update-interval=604800 --kiosk http://localhost:80
+/usr/bin/chromium-browser --incognito --disable-pinch --noerrdialogs --disable-infobars --overscroll-history-navigation=0 --check-for-update-interval=604800 --kiosk http://localhost:80
